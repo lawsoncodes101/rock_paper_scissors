29e8c24940d7fd10cc739a2c96ea376c2c2281e8
lawson101 <lawsoncodes101@gmail.com>
2025-09-22 13:26:09 +0100

    Updated logic for human choice

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const choices = ["rock", "paper", "scissors"]
+const choices = ["rock", "paper", "scissors"]; // must be set to lowercase
 
 function getComputerChoice() {
-    const index = Math.floor(Math.random() * 3); 
+    const index = Math.floor(Math.random() * 3);
     return choices[index];
 }
+
 function getHumanChoice() {
     let choice = "";
+    let count = 0;
     do {
         choice = prompt("Rock, Paper, Scissors?").toLowerCase();
-    } while (!(choices.includes(choice)));
-    
+        count += 1;
+        if (count === 2 && !choices.includes(choice)) {
+            console.log("Invalid inputs!");
+            return false;
+        }
+    } while (!choices.includes(choice));
+    console.log(choice)
     return choice;
 }
+
+getHumanChoice();
