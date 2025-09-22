9c975735da8bf643f3effa06e25396e4e87f90c0
lawson101 <lawsoncodes101@gmail.com>
2025-09-22 12:07:21 +0100

    added logic for computer and human choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Connected!")
+const choices = ["rock", "paper", "scissors"]
+
+function getComputerChoice() {
+    const index = Math.floor(Math.random() * 3); 
+    return choices[index];
+}
+function getHumanChoice() {
+    let choice = "";
+    do {
+        choice = prompt("Rock, Paper, Scissors?").toLowerCase();
+    } while (!(choices.includes(choice)));
+    
+    return choice;
+}
